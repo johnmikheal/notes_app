f8a17d10569b3fa150cb1c1c11aa54fad50636d2
johnmikheal <johnmikheal@gmail.com>
2025-04-12 09:28:08 -0800

    ui done

--- lib/widgets/custom_search_icon.dart
+++ lib/widgets/custom_search_icon.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key});
-
+  const CustomSearchIcon({super.key, required this.icon});
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -11,8 +11,8 @@ class CustomSearchIcon extends StatelessWidget {
       decoration: BoxDecoration(
           color: Colors.white.withValues(alpha: 0.1),
           borderRadius: BorderRadius.circular(16)),
-      child: const Center(
-        child: Icon(Icons.search),
+      child: Center(
+        child: IconButton(onPressed: () {}, icon: Icon(icon)),
       ),
     );
   }
--- lib/widgets/edit_new_body.dart
+++ lib/widgets/edit_new_body.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:tkuo/widgets/custom_app_Bar.dart';
+import 'package:tkuo/widgets/custom_text_field.dart';
 
 class EditNewViewBody extends StatelessWidget {
   const EditNewViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(title: 'Edit Note', icon: Icons.check),
+          SizedBox(
+            height: 50,
+          ),
+          CustomTextField(hint: 'Title'),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hint: 'Content',
+            maxLines: 200,
+          ),
+        ],
+      ),
+    );
   }
 }
